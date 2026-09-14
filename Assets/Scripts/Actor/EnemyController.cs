87f81c3b5358cf2ed0027c55f0ab85c70eb174b5
icahuang <26932284+icahuang@users.noreply.github.com>
2026-09-15 02:58:20 +0800

    chore(Enemy): make the enemy spot function work

--- Assets/Scripts/Actor/EnemyController.cs
+++ Assets/Scripts/Actor/EnemyController.cs
@@ -44,6 +44,9 @@ public class EnemyController : ActorController
             Chase(player, world);
         else
             Patrol(world);
+
+        // 移动后也要更新一次
+        SeesPlayer(player.Cell);
     }
 
     // 发现player后进行追踪的逻辑
@@ -103,33 +106,30 @@ public class EnemyController : ActorController
             case Dir.N:
                 if (delta.x > 0 && Mathf.Abs(delta.x) <= _detectRange) 
                 {
-                    Debug.Log("See u~");
                     _isSpottedPlayer = true;
                 }
                 break;
             case Dir.S:
                 if (delta.x < 0 && Mathf.Abs(delta.x) <= _detectRange)
                 {
-                    Debug.Log("See u~");
                     _isSpottedPlayer = true;
                 }
                 break;
             case Dir.W:
                 if (delta.y > 0 && Mathf.Abs(delta.y) <= _detectRange)
                 {
-                    Debug.Log("See u~");
                     _isSpottedPlayer = true;
                 }
                 break;
             case Dir.E:
                 if (delta.y < 0 && Mathf.Abs(delta.y) <= _detectRange)
                 {
-                    Debug.Log("See u~");
                     _isSpottedPlayer = true;
                 }
                 break;
         }
 
+        if (_isSpottedPlayer) Debug.Log("Chasing start.");
         return;
     }
 
